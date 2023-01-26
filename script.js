690ccba2ecf428537e8e7f2fe81a4072c9e40571
Michel <mikescosta@outlook.com>
2023-01-26 12:34:28 -0300

    Criando linha central

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ const field = {
     w: window.innerWidth,
     h: window.innerHeight,
     draw: function () {//desenha campo
-        canvasCtx.fillStyle = "#286047"
-        canvasCtx.fillRect(0,0, window.innerWidth, window.innerHeight)
+        canvasCtx.fillStyle = "#286047" //cor que definimos para o elemento
+        canvasCtx.fillRect(0,0, window.innerWidth, window.innerHeight) //figura do metodo canvas Rect é retangulo e suas propriedades são x, y, width e heigth
     },
 }
+
+const lineWidth = 15 //variavel que desenha a linha
+
+const x = window.innerWidth / 2 - lineWidth / 2
+const y = 0
+const w = lineWidth
+const h = window.innerHeight
 
 function setup() {
     canvasE1.width = canvasCtx.width = field.w
@@ -16,6 +23,10 @@ function setup() {
 
 function draw(){
     field.draw()
+
+canvasCtx.fillStyle = "#ffffff" // cor dos próximos elementos do canvas
+
+canvasCtx.fillRect(x, y, w, h) //desenha linha central
 }
 
 setup()
